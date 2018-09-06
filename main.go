31fb49f81674479af0d64d3ec9653f31c87f7f19
Zhanliang Liu <zhanliangliu@qq.com>
2018-09-06 16:52:33 +0800

    draft codes

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.com/liuzl/fmr"
+	"github.com/robertkrimen/otto"
+)
+
+var (
+	grammar = flag.String("g", "math.grammar", "grammar file")
+	js      = flag.String("js", "math.js", "javascript file")
+	input   = flag.String("i", "", "file of original text to read")
+	start   = flag.String("start", "result", "start rule")
+)
+
+func main() {
+	flag.Parse()
+	g, err := fmr.GrammarFromFile(*grammar)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	script, err := ioutil.ReadFile(*js)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	vm := otto.New()
+	if _, err = vm.Run(script); err != nil {
+		glog.Fatal(err)
+	}
+
+	var in *os.File
+	if *input == "" {
+		in = os.Stdin
+	} else {
+		in, err = os.Open(*input)
+		if err != nil {
+			glog.Fatal(err)
+		}
+		defer in.Close()
+	}
+	br := bufio.NewReader(in)
+
+	for {
+		line, c := br.ReadString('\n')
+		if c == io.EOF {
+			break
+		}
+		if c != nil {
+			glog.Fatal(c)
+		}
+		line = strings.TrimSpace(line)
+		fmt.Println(line)
+		//p, err := g.EarleyParse(*start, line)
+		//fmt.Println(p, err)
+		//TODO p.GetTrees for (any)
+		//p.GetTrees()
+
+		ps, err := g.EarleyParseMaxAll(line, *start)
+		if err != nil {
+			glog.Fatal(err)
+		}
+		for i, p := range ps {
+			for _, f := range p.GetFinalStates() {
+				trees := p.GetTrees(f)
+				//fmt.Printf("%+v\n", p)
+				fmt.Printf("p%d tree number:%d\n", i, len(trees))
+				for _, tree := range trees {
+					//tree.Print(os.Stdout)
+					sem, err := tree.Semantic()
+					//fmt.Println(sem)
+					if err != nil {
+						glog.Fatal(err)
+					}
+					result, err := vm.Run(sem)
+					if err != nil {
+						glog.Error(err)
+					}
+					rs, _ := result.Export()
+					fmt.Printf("%s = %+v\n", sem, rs)
+					//eval, err := tree.Eval()
+					//fmt.Printf("Eval: %s, Err: %+v\n", eval, err)
+				}
+			}
+		}
+		fmt.Println()
+	}
+}
---
+++ math.js
@@ -0,0 +1,18 @@
+var nf = nf || {};
+nf.math = {};
+
+nf.math.sum = function(x, y) {
+    //console.log(typeof(x), typeof(y));
+    //console.log(x, y);
+    return x + y;
+};
+nf.math.sub = function(x, y) {
+    return x - y;
+}
+nf.math.mul = function(x, y) {
+    return x * y;
+}
+nf.math.neg = function(x) {
+    return -x;
+}
+
