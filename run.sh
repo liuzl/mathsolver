be3c96230a69ab7bc72a3b53e9184764b7e155dc
liuzhanliang <zhanliangliu@qq.com>
2019-05-06 21:25:03 +0800

    pprof

---
+++ run.sh
@@ -0,0 +1,4 @@
+>1000.txt;for ((i=0;i<1000;i++)) do echo "八十的平方与八千八百八十八的和">>1000.txt; done
+go build
+./mathsolver -i 1000.txt -cpuprofile profile.prof > /dev/null
+go tool pprof ./mathsolver profile.prof
